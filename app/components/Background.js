ecff85e35487ba987b83e58f8f08ffc676e4bfb6
Gilbert <65435378+GilbertChuardi@users.noreply.github.com>
2025-03-08 13:26:04 +0700

    remove the image, replace it with css

--- app/components/Background.js
+++ app/components/Background.js
@@ -6,16 +6,20 @@ export default function Background() {
       <div
         className="absolute inset-0 z-10"
         style={{
-          backgroundImage: `radial-gradient(ellipse at center, rgba(0, 0, 0, 0.93) 30%, rgba(0, 0, 0, 0.96) 60%, rgba(0, 0, 0, 1) 80%)`,
+          backgroundImage: `radial-gradient(ellipse at center, rgba(0, 0, 0, 0.3) 30%, rgba(0, 0, 0, 0.8) 60%, rgba(0, 0, 0, 1) 80%)`,
         }}
       ></div>
 
-      <Image
-        src="https://static.vecteezy.com/system/resources/previews/020/335/190/original/modern-abstract-grid-background-template-black-and-white-square-grid-design-vector.jpg"
-        alt="Grid Background"
-        fill
-        className="object-cover z-0"
-      />
+      <div
+        className="absolute inset-0 z-0"
+        style={{
+          backgroundSize: "40px 40px", // Adjust the size of the grid squares
+          backgroundImage: `
+            linear-gradient(to right, rgba(255, 255, 255, 0.1) 1px, transparent 1px),
+            linear-gradient(to bottom, rgba(255, 255, 255, 0.1) 1px, transparent 1px)
+          `,
+        }}
+      ></div>
     </div>
   );
 }

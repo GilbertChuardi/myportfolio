fe63ac28b00e90cf3c593a08cfcb2c3c388bcc56
Gilbert <65435378+GilbertChuardi@users.noreply.github.com>
2025-03-17 13:46:02 +0700

    update contact a bit & fix heroui npm version

--- app/components/ContactMe.js
+++ app/components/ContactMe.js
@@ -24,7 +24,7 @@ export default function Page() {
         <div className="2xl:w-[670px] w-[490px] relative">
           <Border></Border>
         </div>
-        <div className="flex flex-col w-[100%] 2xl:h-[795px] h-[593px] items-center justify-between 2xl:px-28 px-16 z-10">
+        <div className="flex flex-col w-[100%] 2xl:h-[795px] h-[593px] items-center justify-between 2xl:px-28 px-20 z-10 py-10">
           <Input
             isRequired
             errorMessage="Please enter a valid email"
@@ -33,13 +33,18 @@ export default function Page() {
             name="email"
             placeholder="Enter your email"
             type="email"
+            radius="none"
+            variant="bordered"
           />
-          <div className="flex gap-2">
-            <Button color="primary" type="submit">
+          <div className="flex pb-[30px] pl-[100%]">
+            <Button
+              color="primary"
+              type="submit"
+              variant="bordered"
+              radius="none"
+              className="hover:bg-[#006fee] text-white"
+            >
               Submit
-            </Button>
-            <Button type="reset" variant="flat">
-              Reset
             </Button>
           </div>
         </div>
